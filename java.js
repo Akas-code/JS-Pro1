53492ae32eab708501b1654109061affd8685f7f
Akash <codesecoder@gmail.com>
2025-06-27 20:33:52 +0530

    hello

--- java.js
+++ java.js
@@ -9,19 +9,19 @@ boxes.forEach(function (box) {
     // console.log(e.target);
     if (e.target.id === "1st") {
       body.style.backgroundColor = "yellow";
-      document.querySelector("h1").innerHTML = "Anshu I love You";
+      document.querySelector("h1").innerHTML = "I love You";
     }
     if (e.target.id === "2nd") {
       body.style.backgroundColor = "blue";
-      document.querySelector("h1").innerHTML = "Anshu  Tmse bahut pyaar krta hu meri WIFI";
+      document.querySelector("h1").innerHTML = " Tmse bahut pyaar krta hu meri WIFI";
     }
     if (e.target.id === "3rd") {
       body.style.backgroundColor = "red";
-      document.querySelector("h1").innerHTML = "Anshu I love You so much";
+      document.querySelector("h1").innerHTML = "I love You so much";
     }
     if (e.target.id === "4th") {
       body.style.backgroundColor = "orange";
-      document.querySelector("h1").innerHTML = "Anshu Baht pyaar krta hu Tmse";
+      document.querySelector("h1").innerHTML = "Bahut pyaar krta hu Tmse";
     }
   });
 });
